a42a4dae483031dab3261d83a164e642b797a1ab
rvasmenezes <rvasmenezes@gmail.com>
2020-12-22 16:13:24 -0300

    Flutter youtube favoritos

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:favoritos_youtube/blocs/favorite_bloc.dart';
+import 'package:favoritos_youtube/blocs/videos_bloc.dart';
+import 'package:favoritos_youtube/screems/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      bloc: VideosBloc(),
+      child: BlocProvider(
+        bloc: FavoriteBloc(),
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          home: Home(),
+        ),
+      )
+    );
+  }
+}
---
+++ lib/screems/home.dart
@@ -0,0 +1,92 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:favoritos_youtube/blocs/favorite_bloc.dart';
+import 'package:favoritos_youtube/blocs/videos_bloc.dart';
+import 'package:favoritos_youtube/delegates/data_search.dart';
+import 'package:favoritos_youtube/models/video.dart';
+import 'package:favoritos_youtube/screems/favorites.dart';
+import 'package:favoritos_youtube/widgets/video_tile.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    final bloc = BlocProvider.of<VideosBloc>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          height: 25,
+          child: Image.asset("images/youtube_icone.png"),
+
+        ),
+        elevation: 0,
+        backgroundColor: Colors.black87,
+        actions: <Widget>[
+          Align(
+            alignment: Alignment.center,
+            child: StreamBuilder<Map<String, Video>>(
+              stream: BlocProvider.of<FavoriteBloc>(context).outFav,
+              builder: (context, snapshot){
+                if(snapshot.hasData)
+                  return Text("${snapshot.data.length}");
+                else
+                  return Container();
+              },
+            )
+          ),
+          IconButton(
+            icon: Icon(Icons.star),
+            onPressed: (){
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => Favorites()
+              ));
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () async {
+              String result = await showSearch(context: context, delegate: DataSearch());
+              if(result != null) bloc.inSearch.add(result);
+  },
+          )
+        ],
+      ),
+      backgroundColor: Colors.black87,
+      body: StreamBuilder(
+        stream: bloc.outVideos,
+        initialData: [],
+        builder: (context, snapshot){
+
+          if(snapshot.hasData)
+            return ListView.builder(
+              itemBuilder: (context, index){
+
+                if(index < snapshot.data.length){
+                  return VideoTile(snapshot.data[index]);
+                }
+                else if (index > 1){
+                  bloc.inSearch.add(null);
+                  return Container(
+                    height: 40,
+                    width: 40,
+                    alignment: Alignment.center,
+                    child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(Colors.red),),
+                  );
+                }
+                else{
+                  return Container();
+                }
+
+              },
+              itemCount: snapshot.data.length + 1,
+            );
+          else{
+            return Container();
+          }
+        },
+      ),
+
+    );
+  }
+}
